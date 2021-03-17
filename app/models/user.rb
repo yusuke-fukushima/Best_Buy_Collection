e5c2f260945c0bf10657caf58be308f10ca235e9
yusuke-fukushima <usk.fshima.1994@gmail.com>
2021-03-17 22:07:56 +0900

    ゲストログイン機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.name = 'ゲスト'
+      user.password = SecureRandom.urlsafe_base64
+    end
+  end
 
   has_many :items, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
+
   root to: 'homes#top'
   resources :users, only: [:index, :create]
   resources :items, only: [:new, :create]
