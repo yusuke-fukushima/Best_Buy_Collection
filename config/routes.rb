adabfb15fb9641e1b96a75cdf69f657618d5e673
yusuke-fukushima <usk.fshima.1994@gmail.com>
2021-03-18 14:57:54 +0900

    ログイン後のページ遷移設定の変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'items/new'
-  get 'items/create'
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
-  get 'users/update'
   devise_for :users
 
   devise_scope :user do
