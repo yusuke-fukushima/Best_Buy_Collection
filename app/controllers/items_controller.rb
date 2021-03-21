06e34953b333749ddb66c3238f92607f8a8365ed
yusuke-fukushima <usk.fshima.1994@gmail.com>
2021-03-21 23:14:23 +0900

    商品投稿機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,16 +3,26 @@ class ItemsController < ApplicationController
     @items = Item.all
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
   def new
     @item = Item.new
   end
 
   def create
-    @item = Item.new(item_params)
+    @item = current_user.items.new(item_params)
     if @item.save
       redirect_to items_path
     else
       render :new
     end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:purchase_time, :name, :introduction, :item_url)
   end
 end
