110d5901b628396040aafee3a36940867cf2dd0b
yusuke-fukushima <usk.fshima.1994@gmail.com>
2021-04-02 10:38:46 +0900

    indexファイルの削除とindexアクションの削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
-  def index
-    @users = User.all
-  end
-
   def show
     @user = User.find(params[:id])
     @items = Item.all
